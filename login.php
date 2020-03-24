50bfb6d7eaf5327dbeca97bbfb35b1211e1b53a6
Romain Tello <romain.tello@gmail.com>
2020-03-24 12:31:36 +0100

    Add password hashing

--- login.php
+++ login.php
@@ -1,11 +1,10 @@
 <?php
     // Définir la liste des utilisateurs
     $userData = [
-        'toto@test.com' => 'BonjourLesGens',
-        'jeanpierre@test.com' => 'JaimeLesFrites',
-        'michel@test.com' => 'Abcd1234'
+        'toto@test.com' => '$2y$10$5.eeAIZzdNZg9LHdJHZwu.gGqhWm0bv0pR5uPjKKPCXrn4nzs/bdO', // BonjourLesGens
+        'jeanpierre@test.com' => '$2y$10$5HFVyNsIeFZjbrC3ZL2ie.j3ADzBETVXGV.s7JiTlvuSJBp9fI3Yq', // JaimeLesFrites
+        'michel@test.com' => '$2y$10$aBwaQf3wrZSGT5QItN1Nje6HhJx.b79aG79p0iGOG8ES4lTDUgKAm' // Abcd1234
     ];
-
 
     // Si l'e-mail et le mot de passe existent dans les données envoyées à la page
     if (isset($_POST['email']) && isset($_POST['password'])) {
@@ -14,9 +13,9 @@
         $givenPassword = $_POST['password'];
         // Si l'adresse e-mail entrée par l'utilisateur existe dans les données
         if (isset($userData[$email])) {
-            $expectedPassword = $userData[$email];  // => Mot de passe correspondant au nom d'utilisateur rentré dans le formulaire
+            $hash = $userData[$email];  // => Mot de passe correspondant au nom d'utilisateur rentré dans le formulaire
 
-            if ($expectedPassword === $givenPassword) {
+            if (password_verify($givenPassword, $hash)) {
                 echo 'Connexion réussie';
             } else {
                 echo 'Connexion échouée';
